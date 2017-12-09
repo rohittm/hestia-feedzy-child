1ddbfb05f50d07cad6af23d3bea5494c5eec0a42
Rohit Motwani <rohittmotwani@gmail.com>
2017-12-10 00:19:41 +0530

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+  function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'hestia_style',
+      get_template_directory_uri() . '/style.css',
+      array( 'font-awesome', 'bootstrap' )
+    );
+    wp_enqueue_style( 'child-style',
+      get_stylesheet_directory_uri() . '/style.css',
+      array( 'hestia_style' )
+    );
+  }
+  add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+?>
